c4e6a8e6391ed405fdef2447190d49546837e76c
Mel <meljohnpualon@gmail.com>
2022-04-23 07:16:20 +0800

    added keyframes animation on active

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -55,16 +55,17 @@ export default {
   } */
 
   /* ENTER CLASSES */
-  .toast-enter-from {
+  /* .toast-enter-from {
     opacity: 0;
     transform: translateY(-60px);
-  }
+  } */
   /* .toast-enter-to {
     opacity: 1;
     transform: translateY(0);
   } */
   .toast-enter-active {
-    transition: all 0.3s ease-in;
+    /* transition: all 0.3s ease-in; */
+    animation: wobble 0.5s ease;
   }
 
   /* LEAVE CLASSES */
@@ -72,12 +73,37 @@ export default {
     opacity: 1;
     transform: translateY(0);
   } */
-  .toast-leave-to {
+  /* .toast-leave-to {
     opacity: 0;
     transform: translateY(-60px);
   }
   .toast-leave-active {
     transition: all 0.2s ease-in;
-  }
+  } */
 
+  @keyframes wobble {
+    0% {
+      opacity: 0;
+      transform: translateY(-60px);
+    }
+    50% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+    60% {
+      transform: translateX(8px);
+    }
+    70% {
+      transform: translateX(-8px);
+    }
+    80% {
+      transform: translateX(4px);
+    }
+    90% {
+      transform: translateX(-4px);
+    }
+    70% {
+      transform: translateX(0);
+    }
+  }
 </style>
