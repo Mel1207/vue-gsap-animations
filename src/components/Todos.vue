d44d2c24921e2ac40ff102675a4f94e9fe1d815f
Mel <meljohnpualon@gmail.com>
2022-04-24 13:56:52 +0800

    added move and absolute items animation

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -7,7 +7,7 @@
       placeholder="Add a new todo..."
     />
     <div v-if="todos.length">
-      <transition-group tag="ul" name="list">
+      <transition-group tag="ul" name="list" appear>
         <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
           {{ todo.text }}
         </li>
@@ -103,5 +103,11 @@ export default {
   }
   .list-leave-active {
     transition: all 0.5s ease;
+    position: absolute;
+  }
+
+  /* MOVE */
+  .list-move {
+    transition: all 0.3s ease;
   }
 </style>
