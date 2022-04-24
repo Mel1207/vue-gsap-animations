b1fc4b91d2ae2c9ae5b67b352d3fb41f6634a971
Mel <meljohnpualon@gmail.com>
2022-04-24 17:04:50 +0800

    added gsap animations js hooks

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <transition 
+      appear 
+      @before-enter="beforeEnter" 
+      @enter="enter"
+      @after-enter="afterEnter">
+      <h1>About</h1>
+    </transition>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -8,10 +14,30 @@
 </template>
 
 <script>
+import gsap from 'gsap'
 
 export default {
   setup() {
+    const beforeEnter = (el) => {
+      console.log("before enter set initial state")
+      el.style.transform = 'translateY(-60px)'
+      el.style.opacity = 0
+    }
+    const enter = (el, done) => {
+      console.log("starting to enter - make transition")
+      gsap.to(el, {
+        y: 0,
+        opacity: 1,
+        duration: 3,
+        ease: 'bounce.out',
+        onComplete: done
+      })
+    } 
+    const afterEnter = () => {
+      console.log('after enter')
+    }
 
+    return { beforeEnter, enter, afterEnter }
   }
 }
 </script>
